b05ccc59e27022a7e4661c8bf0814c4442484584
rafsanul Hasan <rafsanulhasan@outlook.com>
2020-12-12 13:49:42 +0600

    Solved the problem

---
+++ LabTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabTest
+{
+	public static class Program
+	{
+		private static int CalculateNumberOfPulses(
+			DateTime startDate,
+			DateTime endDate
+		)
+		{
+			var duration = endDate - startDate;
+			return int.Parse(Math.Ceiling(duration.TotalSeconds / 20).ToString());
+		}
+		private static float CalculateRate(
+				DateTime startTime,
+				DateTime endTime
+			)
+				=> startTime.Hour >= 9 && endTime.Hour < 11
+				? 0.30f
+				: 0.20f;
+
+		private static double CalculateBill(
+			DateTime startDate,
+			DateTime endDate
+		)
+		{
+			var duration = CalculateNumberOfPulses(startDate, endDate);
+			var rate = CalculateRate(startDate, endDate);
+			return duration * rate;
+		}
+
+		public static void Main(string[] args)
+		{
+			Console.Write("\nStart Date: ");
+			var startDateString = Console.ReadLine();
+
+			Console.Write("\nEnd Date: ");
+			var endDateString = Console.ReadLine();
+
+
+			startDateString = startDateString.Trim();
+			endDateString = endDateString.Trim();
+			if (!string.IsNullOrEmpty(startDateString) &&
+			    !string.IsNullOrEmpty(endDateString) &&
+			    DateTime.TryParse(startDateString.Trim(), out var startDate) &&
+			    DateTime.TryParse(endDateString.Trim(), out var endDate) &&
+			    endDate > startDate)
+			{
+				var bill = CalculateBill(startDate, endDate);
+				Console.WriteLine($"{bill:N}");
+			}
+			else
+			{
+				Console.WriteLine("invalid input");
+			}
+			Console.ReadKey();
+		}
+	}
+}
